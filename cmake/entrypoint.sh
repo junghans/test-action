213d374472d6e1e4dc55141d6f1cc9fb482f5361
Christoph Junghans <junghans@lanl.gov>
2020-06-09 10:54:17 -0600

    blub

--- cmake/entrypoint.sh
+++ cmake/entrypoint.sh
@@ -4,7 +4,7 @@ echo "$@"
 echo CXX=$CXX
 touch myfile
 echo "::set-output name=cmake_args::-DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug"
-ls
+ls -R
 echo "Begin export"
 export
 echo "End export"
